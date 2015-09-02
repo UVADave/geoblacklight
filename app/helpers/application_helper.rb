ff2c836d8ab75813eb0a9b071b76b4923f771490
Sue Richeson <spr7b@virginia.edu>
2015-09-02 14:02:17 -0400

    The location to return to after an initial Netbadge login was not being stored.  It is now stored using Devise::Controllers::StoreLocation.store_location_for() using the :user scope.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 module ApplicationHelper
+  include Devise::Controllers::StoreLocation
+
+  # Stores the current URL and creates a link to click for 'Login'.
+  def render_login_link
+    store_location_for(:user, request.env['REQUEST_URI'])
+    link_to t('blacklight.header_links.login'), new_user_session_path
+  end
 
 end
