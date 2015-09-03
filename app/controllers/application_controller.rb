c811f839923f69db8e8e21f4e2ceff9c829efedb
Wayne Graham <wayne.graham@virginia.edu>
2015-09-03 13:50:11 -0400

    Fix typo for previous_url symbol

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    session[:prefious_url] || root_path
+    session[:previous_url] || root_path
     #sign_in_url = new_user_session_url
     #if request.referer == sign_in_url
       #super
