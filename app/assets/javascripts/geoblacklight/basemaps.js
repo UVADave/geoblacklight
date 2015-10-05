86bc84ea2816f8f34fc17d31286bedb6ae51556b
Wayne Graham <wayne.graham@virginia.edu>
2015-10-05 12:19:44 -0400

    Update basemap for CORS

--- app/assets/javascripts/geoblacklight/basemaps.js
+++ app/assets/javascripts/geoblacklight/basemaps.js
@@ -32,7 +32,7 @@ GeoBlacklight.Basemaps = {
     //}
   //),
   "World Imagery": L.tileLayer(
-    'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    '//server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 	    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
     }
   ),
@@ -44,7 +44,7 @@ GeoBlacklight.Basemaps = {
     //attribution: 'Tiles &copy; Esri &mdash; Source: US National Park Service',
     //maxZoom: 8
   //}),
-  "National Geographic Map": L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
+  "National Geographic Map": L.tileLayer('//server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
     attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
     maxZoom: 16
   }),
