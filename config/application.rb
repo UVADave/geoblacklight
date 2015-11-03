15b4e7baf6813036304dc2e3a87db4e95215e207
dpg3k <dpg3k@d-137-155-231.bootp.Virginia.EDU>
2015-11-03 11:04:21 -0500

    Support for an empty local_env.yml file

--- config/application.rb
+++ config/application.rb
@@ -30,9 +30,10 @@ module Geoblacklight
     #
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
-      YAML.load(File.open(env_file)).each do |key, value|
+      yaml_file = File.exists?(env_file) ? YAML.load(File.open(env_file)) : nil
+      yaml_file.each do |key, value|
         ENV[key.to_s] = value
-      end if File.exists?(env_file)
+      end unless yaml_file.nil?
     end
 
   end
